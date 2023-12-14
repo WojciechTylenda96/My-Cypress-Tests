786f0e5d0f3c480be56b384dbf42e2137d700a2b
Wojciech Tylenda <wojciech.tylenda.96@gmail.com>
2023-12-14 11:22:55 +0100

    changing folders path

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    excludeSpecPattern: ['**/1-getting-started', '**/2-advanced-examples'],
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
