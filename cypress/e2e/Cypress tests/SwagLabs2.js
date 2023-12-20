f2bae0a0f31d44945175a9547b43469f3b804b49
Wojciech Tylenda <wojciech.tylenda.96@gmail.com>
2023-12-20 17:31:05 +0100

    finished test 'finishing order'

--- cypress/e2e/Cypress tests/SwagLabs2.js
+++ cypress/e2e/Cypress tests/SwagLabs2.js
@@ -12,7 +12,7 @@ const addToCartAndCheckout = () => {
 
 const text = ['Steven', 'Khada', '16-555']
 
-describe('finishing order', () => {
+describe.only('finishing order', () => {
     beforeEach(() => {
         onLoginPage.logIn('standard_user', 'secret_sauce');
         addToCartAndCheckout();
@@ -23,71 +23,25 @@ describe('finishing order', () => {
     })
 
     it('all inputs are correct', () => {
-        cy.get('.checkout_info input').then(input => {
-            cy.wrap(input).eq(0).clear().type(text[0]).should('have.value', text[0]);
-            cy.wrap(input).eq(1).clear().type(text[1]).should('have.value', text[1]);
-            cy.wrap(input).eq(2).clear().type(text[2]).should('have.value', text[2]);
-        });
-        cy.get('#continue').click();
-        cy.get('#finish').click();
-        cy.get('.pony_express').should('have.attr', 'src')
-        cy.get('.complete-header').should('contain', 'Thank you for your order!')
-        cy.get('.complete-text').should('contain', 'Your order has been dispatched, and will arrive just as fast as the pony can get there!')
-        cy.get('#back-to-products').should('exist')
+        onCheckoutPage.inputValues();
     })
 
-    it('all inputs are empty', () => {
-        cy.get('#continue').click();
-        cy.get('.checkout_info input').each((input, index) => {
-            cy.wrap(input).should('have.class', 'error');
-            cy.wrap(input).should('exist', 'error_icon');
-        })
-        cy.get('[data-test="error"]').should('exist')
-    })
-    
-    it('name is empty', () => {
-        cy.get('.checkout_info input').then(input => {
-            cy.wrap(input).eq(0).clear().should('be.empty')
-            cy.wrap(input).eq(1).clear().type(text[1]).should('have.value', text[1])
-            cy.wrap(input).eq(2).clear().type(text[2]).should('have.value', text[2])
-        })
-        cy.get('#continue').click();
-        cy.get('.checkout_info input').eq(0).should('have.class', 'error');
-        cy.get('.checkout_info input').eq(0).should('exist', 'error_icon');
-        cy.get('[data-test="error"]').should('contain', 'Error: First Name is required')
-
-    })
-
-    it('lastname is empty', () => {
-        cy.get('.checkout_info input').then(input => {
-            cy.wrap(input).eq(0).clear().type(text[0]).should('have.value', text[0]);
-            cy.wrap(input).eq(1).clear().should('be.empty');
-            cy.wrap(input).eq(2).clear().type(text[2]).should('have.value', text[2]);
-        })
-        cy.get('#continue').click();
-        cy.get('.checkout_info input').eq(1).should('have.class', 'error');
-        cy.get('.checkout_info input').eq(1).should('exist', 'error_icon');
-        cy.get('[data-test="error"]').should('contain', 'Error: Last Name is required')
-
-    })
-
-    it('postal code is empty', () => {
-        cy.get('.checkout_info input').then(input => {
-            cy.wrap(input).eq(0).clear().type(text[0]).should('have.value', text[0]);
-            cy.wrap(input).eq(1).clear().type(text[1]).should('have.value', text[1]);
-            cy.wrap(input).eq(2).clear().should('be.empty');
-        })
-        cy.get('#continue').click();
-        cy.get('.checkout_info input').eq(2).should('have.class', 'error');
-        cy.get('.checkout_info input').eq(2).should('exist', 'error_icon');
-        cy.get('[data-test="error"]').should('contain', 'Error: Postal Code is required')
-    })
-
-    it.only('testing function', () => {
+    it('one input field is empty, error message should appear ', () => {
         onCheckoutPage.inputValues('firstName');
         onCheckoutPage.inputValues('lastName');
         onCheckoutPage.inputValues('postalCode');
     })
+
+    it('all inputs are empty', () => {
+        cy.get('#continue').click();
+        cy.get('.checkout_info input').each((input) => {
+            cy.wrap(input).should('have.class', 'error');
+            cy.wrap(input).should('exist', 'error_icon');
+        })
+        cy.get('[data-test="error"]').should('contain', 'Error: First Name is required');
+        cy.get('[data-test="error"]').should('exist')
+    })
+    
     
     
 })
--- cypress/support/page_objects/checkoutPage.js
+++ cypress/support/page_objects/checkoutPage.js
@@ -5,17 +5,29 @@ export class CheckoutPage {
     inputValues(emptyField) {
 
         const data = ['Steven', 'Khada', '16-555']
-        
         cy.wrap(data).each((data, index) => {
             cy.get('.checkout_info input').eq(index).type(data)
         })
         cy.get('.checkout_info input').then(inputField => {
             if(emptyField === 'firstName'){
                 cy.wrap(inputField).eq(0).clear();
+                cy.get('#continue').click();
+                cy.get('[data-test="error"]').should('contain', 'Error: First Name is required');
             } else if(emptyField === 'lastName'){
                 cy.wrap(inputField).eq(1).clear();
-            } else{
+                cy.get('#continue').click();
+                cy.get('[data-test="error"]').should('contain', 'Error: Last Name is required');           
+            } else if(emptyField === 'postalCode'){
                 cy.wrap(inputField).eq(2).clear();
+                cy.get('#continue').click();
+                cy.get('[data-test="error"]').should('contain', 'Error: Postal Code is required');
+            } else {
+                cy.get('#continue').click();
+                cy.get('#finish').click();
+                cy.get('.pony_express').should('have.attr', 'src')
+                cy.get('.complete-header').should('contain', 'Thank you for your order!')
+                cy.get('.complete-text').should('contain', 'Your order has been dispatched, and will arrive just as fast as the pony can get there!')
+                cy.get('#back-to-products').should('exist')
             }
         })
     }
